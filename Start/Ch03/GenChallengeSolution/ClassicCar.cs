1319f58df0034b0cd8def06b8ff95a23ca982330
Dat T. Tran <datttrian@gmail.com>
2023-08-21 14:33:05 -0400

    Solution: Programming C# generics

---
+++ Start/Ch03/GenChallengeSolution/ClassicCar.cs
@@ -0,0 +1,17 @@
+﻿namespace GenChallengeSolution;
+
+public class ClassicCar
+{
+    public string m_Make;
+    public string m_Model;
+    public int m_Year;
+    public int m_Value;
+
+    public ClassicCar(string make, string model, int year, int val)
+    {
+        m_Make = make;
+        m_Model = model;
+        m_Year = year;
+        m_Value = val;
+    }
+}
