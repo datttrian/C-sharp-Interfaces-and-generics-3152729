93962c36e5d6025861959b5b31ab50e4054c41b6
Dat T. Tran <datttrian@gmail.com>
2023-08-21 14:12:46 -0400

    Dictionary

---
+++ Start/Ch03/Dictionaries/Program.cs
@@ -0,0 +1,33 @@
+﻿// Create a Dictionary that maps strings to strings
+Dictionary<string, string> fileTypes = new()
+{
+    // Add some file type extensions and their descriptions
+    { ".txt", "Text File" },
+    { ".htm", "HTML Web Page" },
+    { ".html", "HTML Web Page" },
+    { ".css", "Cascading Style Sheet" },
+    { ".xml", "XML Data" }
+};
+
+// TODO: How many key/value pairs are there?
+Console.WriteLine($"There are {fileTypes.Count} key/value pairs");
+
+// TODO: try adding an existing key
+try
+{
+    fileTypes.Add(".htm", "Web Page");
+}
+catch (ArgumentException e)
+{
+    Console.WriteLine("Key already exists");
+}
+
+// TODO: try removing and then finding a key
+fileTypes.Remove(".css");
+Console.WriteLine($"fileTypes contains CSS: {fileTypes.ContainsKey(".css")}");
+// TODO: Dump the contents of the Dictionary
+Console.WriteLine("Dumping dictionary contents: ");
+foreach (KeyValuePair<string, string> kvp in fileTypes)
+{
+    Console.WriteLine($"Key = {kvp.Key}, Value = {kvp.Value}");
+}
